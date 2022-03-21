c33b309e987f7d291637ea3f7f1a79d07a2b3760
Lorena Morais <lorenamorais@alunos.utfpr.edu.br>
2022-03-21 05:52:08 -0300

    add função embralhar deck

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -3,15 +3,28 @@ import Cartao from "./Cartao";
 
 export default function Deck(){
     const perguntas= [
-        {questao: "Pergunta 1", pergunta: "O que é JSX?", resposta: "Uma extensão de linguagem do JavaScript"},
-        {questao: "Pergunta 2", pergunta: "O React é __", resposta: "uma biblioteca JavaScript para construção de interfaces"},
-        {questao: "Pergunta 3", pergunta: "Componentes devem iniciar com __", resposta: "letra maiúscula"},
-        {questao: "Pergunta 4", pergunta: "Podemos colocar __ dentro do JSX", resposta: "expressões"},
-        {questao: "Pergunta 5", pergunta: "O ReactDOM nos ajuda __", resposta: "interagindo com a DOM para colocar componentes React na mesma"},
-        {questao: "Pergunta 6", pergunta: "Usamos o npm para __", resposta: "gerenciar os pacotes necessários e suas dependências"},
-        {questao: "Pergunta 7", pergunta: "Usamos props para __", resposta: "passar diferentes informações para componentes "},
-        {questao: "Pergunta 8", pergunta: "Usamos estado (state) para __", resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
+        {pergunta: "O que é JSX?", resposta: "Uma extensão de linguagem do JavaScript"},
+        {pergunta: "O React é __", resposta: "uma biblioteca JavaScript para construção de interfaces"},
+        {pergunta: "Componentes devem iniciar com __", resposta: "letra maiúscula"},
+        {pergunta: "Podemos colocar __ dentro do JSX", resposta: "expressões"},
+        {pergunta: "O ReactDOM nos ajuda __", resposta: "interagindo com a DOM para colocar componentes React na mesma"},
+        {pergunta: "Usamos o npm para __", resposta: "gerenciar os pacotes necessários e suas dependências"},
+        {pergunta: "Usamos props para __", resposta: "passar diferentes informações para componentes "},
+        {pergunta: "Usamos estado (state) para __", resposta: "dizer para o React quais informações quando atualizadas devem renderizar a tela novamente"}
     ];
+    perguntas.sort(embaralhar)
+
+    function embaralhar(){
+        return Math.random() - 0.5;
+    }
+
+    function opçaoPergunta(){
+        for(let i = 0; i < perguntas.length; i++){
+            perguntas[i].questao = `Pergunta ${i+1}`;
+        }
+    }
+    opçaoPergunta();
+    
     return (
         <div className="deck">
             {perguntas.map(cartao => <Cartao questao={cartao.questao} pergunta={cartao.pergunta} resposta={cartao.resposta}/>)}
