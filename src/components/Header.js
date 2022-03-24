ddcc66dd55288fe69f660a978ab1478ad487baf2
Gabriel Oliveira <gabrielh-oliveira@hotmail.com>
2022-03-24 09:18:11 -0300

    Preparando revisão de código

--- src/components/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return (
-        <header>
-            <img className="logo-pequena" src="./assets/logo-pequeno.png" alt="logo ZapRecall"/>
-            <h1>ZapRecall</h1>
-        </header>
-    )
-}
